2596f3ae598f4e0af748eabedee76126609132de
Dinimini <egon.dini@gmail.com>
2025-10-08 14:21:03 +0200

    PHASE: basic project structure created: model, repository, service, controller

---
+++ src/test/java/pa/minicasino/MiniCasinoApplicationTests.java
@@ -0,0 +1,13 @@
+package pa.minicasino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCasinoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
